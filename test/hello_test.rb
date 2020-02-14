6fe8014b58962629c50606e2698e92403c31dac1
k2works <kakimomokuri@gmail.com>
2020-02-14 17:47:55 +0900

    test: 仮実装

--- test/hello_test.rb
+++ test/hello_test.rb
@@ -2,16 +2,6 @@ require 'minitest/reporters'
 Minitest::Reporters.use!
 require 'minitest/autorun'
 
-class HelloTest < Minitest::Test
-  def test_greeting
-    assert_equal 'hello world', greeting
-  end
-end
-
-def greeting
-  'hello world'
-end
-
 # 仕様
 # SNS管理者として
 # ユーザーを管理できるようにしたい
@@ -20,3 +10,17 @@ end
 # TODOリスト
 # - [ ] IDと名前を持ったユーザーを作成する
 # - [ ] ユーザー名を更新する
+
+class UserTest < Minitest::Test
+  def test_ユーザーが作成される
+    user = User.new('Bob')
+    assert_equal user.name, 'Bob'
+  end
+end
+
+class User
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
