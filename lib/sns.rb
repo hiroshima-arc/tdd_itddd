019c905d9c4d78d6ff3102a6c99319544f3c13dd
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-05-22 18:55:00 +0900

    refactor: クラスの抽出

--- lib/sns.rb
+++ lib/sns.rb
@@ -3,4 +3,5 @@
 require './lib/user_id.rb'
 require './lib/user_name.rb'
 require './lib/user.rb'
+require './lib/user_service.rb'
 require 'sqlite3'
---
+++ test/user_service_test.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require './test/test_helper'
+require './lib/sns.rb'
+
+class UserServiceTest < Minitest::Test
+end
